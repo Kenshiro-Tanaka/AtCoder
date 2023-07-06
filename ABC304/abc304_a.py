74af6dede821618a4312d8d956db42164dca34a5
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2023-07-06 19:27:34 +0900

    Update abc304_a.py

--- ABC304/abc304_a.py
+++ ABC304/abc304_a.py
@@ -3,14 +3,22 @@ l = [list(map(str, input().split())) for i in range(N)]
 
 # 一番若い人のindexが欲しい
 tmp = 10**10 # Aの最大値より1桁多い
-index = 0
+ind = 0
 for i in range(N):
     if tmp > int(l[i][1]):
         tmp = int(l[i][1])
-        index = i
+        ind = i
     
-L = l[index:] + l[:index]
+L = l[ind:] + l[:ind]
 
 # この書き方できるのか
 for name, age in L:
     print(name)
+
+
+# 別解
+# m = min(A)
+# ind = A.index(m) 重複していないならindexが一意に返ってくる
+# S.extend(S) Sの後ろにSを追加して円卓に
+# for i in range(ind, ind + N):
+#     print(S[i])
