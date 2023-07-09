e8b026841c5e920174d22a731501e959ad85cbb1
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2023-07-10 05:24:37 +0900

    Create abc297_a.py

---
+++ ABC297/abc297_a.py
@@ -0,0 +1,10 @@
+N, D = map(int, input().split())
+T = list(map(int, input().split()))
+
+ans = -1
+for i in range(N - 1): # N-1まで
+    if T[i + 1] - T[i] <= D:
+        ans = T[i + 1]
+        break
+
+print(ans)
