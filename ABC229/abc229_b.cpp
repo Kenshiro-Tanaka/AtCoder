f2a693bdb865edb8a14c627befe530641e2ee7d6
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2023-07-09 10:11:02 +0900

    Create abc229_b.cpp

---
+++ ABC229/abc229_b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+int N; cin >> N;
+int H, W; cin >> H >> W;
+string str1; cin >> str1;
+vector<int> A(200010, 0);
+for (int i = 0; i < N; i++) cin >> A.at(i);
+vector<vector<int>> data1(H, vector<int>(W));
+for (int i = 0; i < H; i++) {for (int j = 0; j < W; j++)
+{cin >> data1.at(i).at(j);}}
+bool flag = false;
+*/
+int main() {
+    long long A, B;
+    cin >> A >> B;
+
+    string flag = "Easy";
+    int a1, b1;
+    while (A != 0 and B != 0) {
+        a1 = A % 10;  // 1の位は10で割ったあまりで
+        b1 = B % 10;
+        if (a1 + b1 >= 10) {
+            flag = "Hard";
+            break;
+        } else {
+            A /= 10;  // 商を10で割って1桁ずつ減らしていく
+            B /= 10;
+        }
+    }
+
+    cout << flag;
+    return 0;
+}
