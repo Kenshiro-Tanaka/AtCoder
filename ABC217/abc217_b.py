fcebd9fb231d5c86b1c1f3c18368fe4484af1abb
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2023-08-11 05:01:49 +0900

    Update abc217_b.py

--- ABC217/abc217_b.py
+++ ABC217/abc217_b.py
@@ -8,3 +8,13 @@ S.remove(S2)
 S.remove(S3)
 
 print(''.join(S))
+
+
+# 別解 # 答え集合から取り出して入力配列に無ければ出力して終わり
+# S = [input() for _ in range(3)]
+
+# Ans = ["ABC", "ARC", "AGC", "AHC"]
+# for i in Ans:
+#     if i not in S:
+#         print(i)
+#         break
