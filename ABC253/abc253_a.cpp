bc20aa7cfd5a4b4e521be910ae9a622b84b87707
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2023-08-22 12:54:35 +0900

    Create abc253_a.cpp

---
+++ ABC253/abc253_a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+int N; cin >> N;
+int H, W; cin >> H >> W;
+string str1; cin >> str1;
+vector<int> A(N), B(N);
+for (int i = 0; i < N; i++) cin >> A.at(i) >> B.at(i);
+vector<vector<int>> data1(H, vector<int>(W));
+for (int i = 0; i < H; i++) {for (int j = 0; j < W; j++)
+{cin >> data1.at(i).at(j);}}
+bool flag = false;
+for (auto&& a:A) cin >> a;
+*/
+int main() {
+    vector<int> A(3);
+    for (int i = 0; i < 3; i++) cin >> A.at(i);
+
+    int b = A.at(1);
+    sort(A.begin(), A.end());
+    cout << (b == A.at(1) ? "Yes" : "No");
+
+    return 0;
+}
