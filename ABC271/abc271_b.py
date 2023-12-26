fc87600af9f48f8f4a24ba9d4906a102214dd626
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2023-12-26 18:46:27 +0900

    Update abc271_b.py

--- ABC271/abc271_b.py
+++ ABC271/abc271_b.py
@@ -2,7 +2,7 @@ N, Q = map(int, input().split())
 a = [[] for _ in range(N)]
 for i in range(N):
     a[i] = list(map(int, input().split()))
-print(a)
+
 for _ in range(Q):
     s, t = map(int, input().split())
     print(a[s-1][t])
