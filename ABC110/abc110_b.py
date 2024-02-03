171691d2ed86203cc06fb5312cda19205bbdde94
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2024-02-03 10:35:20 +0900

    Create abc110_b.py

---
+++ ABC110/abc110_b.py
@@ -0,0 +1,10 @@
+N, M, X, Y = map(int, input().split())
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+
+for Z in range(X+1, Y+1, 1):
+    if max(x) < Z and min(y) >= Z:
+        print("No War")
+        exit()
+    
+print("War")
