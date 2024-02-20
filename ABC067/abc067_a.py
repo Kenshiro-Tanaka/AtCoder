170337a9f4c8dbab57ba1d2f5b07eb133f9c0d10
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2024-02-20 15:31:46 +0900

    Create abc067_a.py

---
+++ ABC067/abc067_a.py
@@ -0,0 +1,10 @@
+A, B = map(int, input().split())
+
+if A+B < 3:
+    print("Impossible")
+else:
+    if A%3 == 0 or B%3 == 0 or (A+B)%3 == 0:
+        print("Possible")
+    else:
+        print("Impossible")
+        
