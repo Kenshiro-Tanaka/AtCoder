e96949d3bd105acb14dfc2d30ad79211e034d977
Kenshiro-Tanaka <99978726+Kenshiro-Tanaka@users.noreply.github.com>
2024-02-27 10:44:53 +0900

    Update abc193_c.py

--- ABC193/abc193_c.py
+++ ABC193/abc193_c.py
@@ -1,10 +1,10 @@
 N = int(input())
 
-l = []
+st = set()
 for a in range(2, 10**5+1, 1):
     b = 2
     while a**b <= N:
-            l.append(a**b)
+            st.add(a**b)
             b += 1
 
-print(N-len(list(set(l)))) # 2^4と4^2みたいな被りがある
+print(N-len(st)) # 2^4と4^2みたいな被りがある
